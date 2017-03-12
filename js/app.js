00b507e85a0c0c4505f5cebc7dcae91965e8f0c2
amanfojnr <kenmarch@zoho.com>
2017-03-12 12:19:03 +0000

    added cors to fix insecure http

--- js/app.js
+++ js/app.js
@@ -9,22 +9,22 @@ $("document").ready(function() {
 
   }
 
-// on clicking the quote button
-$("#generate").on("click", function() {
-  var quote = $("#quote"); // load quote element
-  var author = $("#author"); // load author element
+  // on clicking the quote button
+  $("#generate").on("click", function() {
+    var quote = $("#quote"); // load quote element
+    var author = $("#author"); // load author element
 
-// get json object using storm quote api
-$.getJSON("http://quotes.stormconsultancy.co.uk/random.json",
-  function(json){
-    // change quote and author to new quote from api
-    quote.text(json.quote);
-    author.text(json.author);
+    // get json object using storm quote api
+    $.getJSON("https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/random.json",
+      function(json) {
+        // change quote and author to new quote from api
+        quote.text(json.quote);
+        author.text(json.author);
 
-    // replace pretweet url with new url
-    var tweetLink = "http://twitter.com/intent/tweet?text="
-     $("#tweet").attr("href", generateTweet(tweetLink, json.quote, json.author));
+        // replace pretweet url with new url
+        var tweetLink = "https://twitter.com/intent/tweet?text="
+        $("#tweet").attr("href", generateTweet(tweetLink, json.quote, json.author));
+      });
   });
-});
 
 });
